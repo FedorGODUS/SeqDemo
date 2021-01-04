53186da21bbd5e252a14e8119fceea0f66b658e8
fedorGOD <59053843+fedorGODUS@users.noreply.github.com>
2021-01-04 17:39:35 +0300

    5 Commit. Adding users and applying auth for them

--- src/main/java/Andersen/SeqDemo/Config/SecurityConfig.java
+++ src/main/java/Andersen/SeqDemo/Config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package Andersen.SeqDemo.Config;
 
+import Andersen.SeqDemo.Model.Permission;
 import Andersen.SeqDemo.Model.Role;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Bean;
@@ -27,9 +28,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/").permitAll()
-                .antMatchers(HttpMethod.GET, "/api/**").hasAnyRole(Role.ADMIN.name(), Role.USER.name())
-                .antMatchers(HttpMethod.POST, "/api/**").hasRole(Role.ADMIN.name())
-                .antMatchers(HttpMethod.DELETE, "/api/**").hasRole(Role.ADMIN.name())
+                .antMatchers(HttpMethod.GET, "/api/**").hasAuthority(Permission.DEVELOPERS_READ.getPermission())
+                .antMatchers(HttpMethod.POST, "/api/**").hasAuthority(Permission.DEVELOPERS_WRITE.getPermission())
+                .antMatchers(HttpMethod.DELETE, "/api/**").hasAuthority(Permission.DEVELOPERS_WRITE.getPermission())
                 .anyRequest()
                 .authenticated()
                 .and()
@@ -43,12 +44,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             User.builder()
                     .username("admin")
                     .password(passwordEncoder().encode("admin"))
-                    .roles(Role.ADMIN.name())
+                    .authorities(Role.ADMIN.getAuthorities())
                     .build(),
                 User.builder()
                         .username("user")
                         .password(passwordEncoder().encode("user"))
-                        .roles(Role.USER.name())
+                        .authorities(Role.USER.getAuthorities())
                         .build()
         );
     }
