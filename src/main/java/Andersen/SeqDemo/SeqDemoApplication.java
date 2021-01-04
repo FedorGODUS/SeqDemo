9a04ea58e56c515e7871aeac486f0da0a8435328
fedorGOD <59053843+fedorGODUS@users.noreply.github.com>
2021-01-04 14:37:17 +0300

    1 Commit. Create an entity

---
+++ src/main/java/Andersen/SeqDemo/SeqDemoApplication.java
@@ -0,0 +1,14 @@
+package Andersen.SeqDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeqDemoApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SeqDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Andersen/SeqDemo/SeqDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package Andersen.SeqDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeqDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
